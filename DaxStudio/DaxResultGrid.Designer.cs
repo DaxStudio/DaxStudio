62f82ee175c430326a35909bcc390feacf30790b
ptebraak_cp <73276f85fdf700e1414c2d3c90f2e372bda1623e4ycAfmEW>
2012-07-05 04:40:22 +0000

    Add DaxResultGrid to Project (SVN)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS01]$/daxstudio/DaxStudio;C98593

---
+++ DaxStudio/DaxResultGrid.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace DaxStudio
+{
+    partial class DaxResultGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DaxResultGrid));
+            this.daxGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.daxGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // daxGrid
+            // 
+            this.daxGrid.AllowUserToAddRows = false;
+            this.daxGrid.AllowUserToDeleteRows = false;
+            this.daxGrid.AllowUserToOrderColumns = true;
+            this.daxGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.daxGrid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.daxGrid.Location = new System.Drawing.Point(13, 13);
+            this.daxGrid.Name = "daxGrid";
+            this.daxGrid.Size = new System.Drawing.Size(390, 260);
+            this.daxGrid.TabIndex = 0;
+            // 
+            // DaxResultGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 281);
+            this.Controls.Add(this.daxGrid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DaxResultGrid";
+            this.Text = "Query Results";
+
+            this.ResizeEnd += new System.EventHandler(this.DaxResultGrid_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.daxGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView daxGrid;
+    }
+}
---
+++ DaxStudio/DaxResultGrid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DaxStudio
+{
+    public partial class DaxResultGrid : Form
+    {
+
+        private int WidthBuffer = 40;
+        private int HeightBuffer = 60;
+
+        public DaxResultGrid()
+        {
+            InitializeComponent();
+        }
+
+
+        public void BindResults(System.Data.DataTable ResultTable)
+        {
+            daxGrid.DataSource = ResultTable;
+            daxGrid.Refresh();
+
+            if (daxGrid.Width < this.Width - WidthBuffer )
+                this.Width = daxGrid.Width + WidthBuffer;
+
+        }
+
+        private void DaxResultGrid_ResizeEnd(object sender, EventArgs e)
+        {
+            daxGrid.Width = this.Width - WidthBuffer;
+            daxGrid.Height = this.Height - 60;
+
+
+        }
+
+
+
+
+
+    }
+}
