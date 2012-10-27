14aadd98f2cac5223e2d1f2a692423f6a5151387
ptebraak_cp <73276f85fdf700e1414c2d3c90f2e372bda1623e4ycAfmEW>
2012-10-27 22:20:41 +0000

    Add ConnectionDialog.xaml & ConnectionDialog.xaml.cs (WPF Connection Dialogue)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS01]$/daxstudio/DaxStudio;C100169

---
+++ DaxStudio/ConnectionDialog.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf_testimng
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ConnectionDialog : Window
+    {
+
+
+        public ConnectionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void rdoTabularServer_Checked(object sender, RoutedEventArgs e)
+        {
+            lblPowerPivotUnavailable.Visibility = Visibility.Hidden;
+        }
+    }
+}
