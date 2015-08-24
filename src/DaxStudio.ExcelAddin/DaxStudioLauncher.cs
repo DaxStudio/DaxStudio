2ffd2501ca716fda6b50bda82a90a9c205db192b
Darren Gosbell <dgosbell@users.noreply.github.com>
2015-08-25 06:47:48 +1000

    Adding class to facilitate VBA integration

---
+++ src/DaxStudio.ExcelAddin/DaxStudioLauncher.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using DaxStudio.ExcelAddin;
+
+namespace DaxStudio
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class DaxStudioLauncher:IDaxStudioLauncher
+    {
+        private readonly IDaxStudioLauncher _launcher;
+        public DaxStudioLauncher(IDaxStudioLauncher launcher)
+        {
+            _launcher = launcher;
+        }
+        public void Launch()
+        {
+            _launcher.Launch();
+        }
+    }
+}
