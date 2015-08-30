1f55e80d4269b7bae955e567b9b2fe83a04b5d12
Darren Gosbell <dgosbell@users.noreply.github.com>
2015-08-31 07:03:12 +1000

    Fixing tracing busy status when doing clear cache and run. Updating QueryHistory styles

---
+++ src/DaxStudio.UI/Converters/MaxLinesConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DaxStudio.UI.Converters
+{
+    public class MaxLinesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string text = value as string;
+            int maxLines;
+            int.TryParse(parameter.ToString(), out maxLines);
+            if (text != null && maxLines > 0)
+            {
+                var lines = text.Split('\n');
+                if (lines.Length > maxLines)
+                {
+                    return string.Join("\n", lines.Take((int)maxLines).ToArray<string>()) + "…";
+                }
+                return text;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
