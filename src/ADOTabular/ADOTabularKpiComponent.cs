cb9bf39c92daf995e27c7029a872ced29ffde0b7
RuiRomano <ruiromano@outlook.com>
2015-12-09 10:42:12 +0000

    Bug when Define Measure over a KPI

--- src/ADOTabular/ADOTabularKpiComponent.cs
+++ src/ADOTabular/ADOTabularKpiComponent.cs
@@ -16,16 +16,17 @@ namespace ADOTabular
     {
         public ADOTabularKpiComponent(ADOTabularColumn column, KpiComponentType type)
         {
-            _column = column;
+            Column = column;
             ComponentType = type;
         }
-        private ADOTabularColumn _column;
+        // Need to be Public to grab the KPI measure expressions
+        public ADOTabularColumn Column;
         public KpiComponentType ComponentType { get; set; }
 
         public string Caption { get { return ComponentType.ToString(); } }
-        public string DaxName { get { return _column.DaxName; } }
+        public string DaxName { get { return Column.DaxName; } }
 
-        public string DataTypeName { get { return _column.DataTypeName; } }
+        public string DataTypeName { get { return Column.DataTypeName; } }
 
     }
 }
