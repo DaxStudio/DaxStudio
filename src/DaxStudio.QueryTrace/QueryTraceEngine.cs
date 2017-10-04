7491c314f2f600c72eec56ca9fa7d7a168b088ef
dgosbell <dgosbell@users.noreply.github.com>
2017-10-05 09:56:58 +1100

    Adding try/catch when disposing trace

--- src/DaxStudio.QueryTrace/QueryTraceEngine.cs
+++ src/DaxStudio.QueryTrace/QueryTraceEngine.cs
@@ -423,7 +423,14 @@ namespace DaxStudio.QueryTrace
         {
             if (_trace == null) return; // exit here if trace has already been disposed
             _trace.OnEvent -= OnTraceEventInternal;
-            _trace.Drop();
+            try
+            {
+                _trace.Drop();
+            } catch (Exception ex)
+            {
+                Log.Error(ex, "{Class} {Method} Exception while dropping query trace {message}", "QueryTraceEngine", "DisposeTrace", ex.Message);
+            }
+
             _trace.Dispose();
             _trace = null;
         }
