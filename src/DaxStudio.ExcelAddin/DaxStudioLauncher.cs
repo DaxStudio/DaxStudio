35ab8d318d0887c96a1962511a88a3ac79337a61
dgosbell <dgosbell@users.noreply.github.com>
2017-10-05 09:52:41 +1100

    Updating VBA launch class - fixing #14

--- src/DaxStudio.ExcelAddin/DaxStudioLauncher.cs
+++ src/DaxStudio.ExcelAddin/DaxStudioLauncher.cs
@@ -1,14 +1,9 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 using DaxStudio.ExcelAddin;
 
 namespace DaxStudio
 {
+    // This class allows for VBA to launch the DAX Studio UI
     [ComVisible(true)]
     [ClassInterface(ClassInterfaceType.None)]
     public class DaxStudioLauncher:IDaxStudioLauncher
@@ -18,10 +13,11 @@ namespace DaxStudio
         {
             _launcher = launcher;
         }
-        //public void Launch()
-        //{
-        //    _launcher.Launch();
-        //}
+
+        public void Launch()
+        {
+            _launcher.Launch(false);
+        }
 
         public void Launch(bool enableLogging)
         {
