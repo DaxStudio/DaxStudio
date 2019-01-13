036afba04583074f6b8c690913ba81014e1518d1
dgosbell <dgosbell@users.noreply.github.com>
2019-01-14 09:39:22 +1100

    Updating to .Net 4.7.1 to enable using TLS 1.2

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © DAX Studio 2016")] 
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion(    "2.8.0.111")]
-[assembly: AssemblyFileVersion("2.8.0.111")]
+[assembly: AssemblyVersion(    "2.8.0.112")]
+[assembly: AssemblyFileVersion("2.8.0.112")]
 
 
