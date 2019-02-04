cd11207979fb954770e156a78d2a9c9db44bf3a6
dgosbell <dgosbell@users.noreply.github.com>
2019-02-04 19:58:30 +1100

    Fixing #147 visibility of external tools

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © DAX Studio 2016")] 
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion(    "2.8.2.116")]
-[assembly: AssemblyFileVersion("2.8.2.116")]
+[assembly: AssemblyVersion(    "2.8.3.117")]
+[assembly: AssemblyFileVersion("2.8.3.117")]
 
 
