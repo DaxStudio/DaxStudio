a11785a4a146c66aa4934f9e94f2931669fffa4b
Darren Gosbell <dgosbell@users.noreply.github.com>
2019-06-03 10:56:50 +1000

    Removing Preview flag from import PBI Perf Data

--- src/CommonAssemblyVersion.cs
+++ src/CommonAssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion(    "2.9.0.121")]
-[assembly: AssemblyFileVersion("2.9.0.121")]
+[assembly: AssemblyVersion(    "2.9.0.122")]
+[assembly: AssemblyFileVersion("2.9.0.122")]
