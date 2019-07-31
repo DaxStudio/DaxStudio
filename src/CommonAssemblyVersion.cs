06f0450846b25112cf39748e14dfda1a8ec2312b
dgosbell <dgosbell@users.noreply.github.com>
2019-07-31 22:59:33 +1000

    fixing light theme compile

--- src/CommonAssemblyVersion.cs
+++ src/CommonAssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion(    "2.9.3.128")]
-[assembly: AssemblyFileVersion("2.9.3.128")]
+[assembly: AssemblyVersion(    "2.9.3.129")]
+[assembly: AssemblyFileVersion("2.9.3.129")]
