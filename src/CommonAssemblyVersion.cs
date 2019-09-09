906c5c3e050cb098413ca5bf9e3d9145be944794
dgosbell <dgosbell@users.noreply.github.com>
2019-09-10 09:25:33 +1000

    Updating version number

--- src/CommonAssemblyVersion.cs
+++ src/CommonAssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion(    "2.9.3.130")]
-[assembly: AssemblyFileVersion("2.9.3.130")]
+[assembly: AssemblyVersion(    "2.9.3.131")]
+[assembly: AssemblyFileVersion("2.9.3.131")]
