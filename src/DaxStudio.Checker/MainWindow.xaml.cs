a3001ee6a6cf2f77c6148c3b400ddad94ef111b8
dgosbell <dgosbell@users.noreply.github.com>
2020-05-08 09:47:16 +1000

    removing MS library checks from checker

--- src/DaxStudio.Checker/MainWindow.xaml.cs
+++ src/DaxStudio.Checker/MainWindow.xaml.cs
@@ -35,10 +35,10 @@ namespace DaxStudio.CheckerApp
                 checker.CheckOSInfo();
                 checker.CheckScreenInfo();
                 checker.CheckNetFramework();
-                checker.CheckLocalLibrary("AMO", "Microsoft.AnalysisServices.dll");
-                checker.CheckLibraryExact("Microsoft.AnalysisServices",true);
-                checker.CheckLocalLibrary("ADOMD.NET", "Microsoft.AnalysisServices.AdomdClient.dll");
-                checker.CheckLibraryExact("Microsoft.AnalysisServices.AdomdClient", true);
+                //checker.CheckLocalLibrary("AMO", "Microsoft.AnalysisServices.dll");
+                //checker.CheckLibraryExact("Microsoft.AnalysisServices",true);
+                //checker.CheckLocalLibrary("ADOMD.NET", "Microsoft.AnalysisServices.AdomdClient.dll");
+                //checker.CheckLibraryExact("Microsoft.AnalysisServices.AdomdClient", true);
                 //checker.CheckLibrary("AMO",       "Microsoft.AnalysisServices, Version={0}.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91");
                 //checker.CheckLibrary("ADOMD.NET", "Microsoft.AnalysisServices.AdomdClient, Version={0}.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91");
                 checker.CheckDaxStudioBindings();
