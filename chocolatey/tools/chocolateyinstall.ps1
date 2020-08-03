c4f28b032ae93be9166a7d54aaeeb47a0a203da4
dgosbell <dgosbell@users.noreply.github.com>
2020-08-04 09:14:06 +1000

    updating chocolatey build

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -3,15 +3,24 @@
 $toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
 $fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.exe').FullName
 
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/DaxStudio/DaxStudio/releases/download/2.9.1/DaxStudio_2_9_1_setup.exe'
+$checksum   = 'D373C94BE5E3F87132B1327C4FC288FC868FA1FDD58127A3CD792D6314BA5C91'
+
 $packageArgs = @{
-   packageName    = $env:ChocolateyPackageName
-   fileType       = 'EXE' 
-   file           = "$fileLocation"
-   softwareName   = "$env:ChocolateyPackageName*"
-   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-   validExitCodes = @(0)
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url
+  softwareName  = 'DAX Studio'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  checksum64    = $checksum
+  checksumType64= 'sha256'
+  validExitCodes= @(0)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
-Install-ChocolateyInstallPackage @packageArgs
-
-New-Item "$fileLocation.ignore" -Type file -Force | Out-Null
+Install-ChocolateyPackage @packageArgs
