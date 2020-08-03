b64d4f94c39aa3d91c641fb618904de400472f2d
dgosbell <dgosbell@users.noreply.github.com>
2020-08-03 19:48:38 +1000

    adding chocolatey package files

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$fileLocation = (Get-ChildItem -Path $toolsDir -Filter '*.exe').FullName
+
+$packageArgs = @{
+   packageName    = $env:ChocolateyPackageName
+   fileType       = 'EXE' 
+   file           = "$fileLocation"
+   softwareName   = "$env:ChocolateyPackageName*"
+   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+   validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+New-Item "$fileLocation.ignore" -Type file -Force | Out-Null
