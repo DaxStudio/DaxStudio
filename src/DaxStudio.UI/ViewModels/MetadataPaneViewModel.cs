05dec1c5669884c45d6801821e22a20945cfe107
dgosbell <dgosbell@users.noreply.github.com>
2020-10-28 13:55:19 +1100

    Update MetadataPaneViewModel.cs

--- src/DaxStudio.UI/ViewModels/MetadataPaneViewModel.cs
+++ src/DaxStudio.UI/ViewModels/MetadataPaneViewModel.cs
@@ -253,6 +253,7 @@ namespace DaxStudio.UI.ViewModels
                 if (value == _modelList)
                     return;
                 _modelList = value;
+                SelectedModel = _modelList.BaseModel;
                 NotifyOfPropertyChange(() => ModelList);
                 NotifyOfPropertyChange(() => SelectedModel);
             }
