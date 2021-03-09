6b4534b23803546268b720c446b160fb1ae5dba1
dgosbell <dgosbell@users.noreply.github.com>
2021-03-09 11:30:09 +1100

    Hopefully fixing IsApplicationActive bug

--- src/DaxStudio.UI/Utils/ApplicationHelper.cs
+++ src/DaxStudio.UI/Utils/ApplicationHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,11 +13,14 @@ namespace DaxStudio.UI.Utils
     public static class ApplicationHelper
     {
 
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
 
 
-        public static bool IsApplicationActive()
+        public static bool IsApplicationActiveOld()
         {
             var foregroundHwnd = GetForegroundWindow();
             foreach (var wnd in Application.Current.Windows.OfType<Window>())
@@ -25,6 +30,24 @@ namespace DaxStudio.UI.Utils
             }
             return false;
         }
+
+
+        /// <summary>Returns true if the current application has focus, false otherwise</summary>
+        public static bool IsApplicationActive()
+        {
+            var activatedHandle = GetForegroundWindow();
+            if (activatedHandle == IntPtr.Zero)
+            {
+                return false;       // No window is currently activated
+            }
+
+            var procId = Process.GetCurrentProcess().Id;
+            GetWindowThreadProcessId(activatedHandle, out var activeProcId);
+
+            return activeProcId == procId;
+        }
+
+
 
     }
 }
