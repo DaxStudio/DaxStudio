a0a71f895e7a3591a9c449306c8aaab398225e60
Darren Gosbell <dgosbell@users.noreply.github.com>
2021-06-14 19:36:50 +1000

    improving styling of auto-generate button

--- src/DaxStudio.UI/AttachedProperties/GridSplitterController.cs
+++ src/DaxStudio.UI/AttachedProperties/GridSplitterController.cs
@@ -27,12 +27,31 @@ namespace DaxStudio.UI.AttachedProperties
                 typeof(DependencyObject),
                 new UIPropertyMetadata(null, OnWatchChanged));
 
+        public static double GetHeightRatio(DependencyObject obj)
+        {
+            return (double)obj.GetValue(HeightRatioProperty);
+        }
+
+        public static void SetHeightRatio(DependencyObject obj, double value)
+        {
+            obj.SetValue(HeightRatioProperty, value);
+        }
+
+        public static readonly DependencyProperty HeightRatioProperty =
+            DependencyProperty.RegisterAttached(
+                "HeightRatio",
+                typeof(double),
+                typeof(DependencyObject),
+                new UIPropertyMetadata(1.0, OnWatchChanged));
+
+
         private static void OnWatchChanged(DependencyObject obj,
-    DependencyPropertyChangedEventArgs e)
+                                           DependencyPropertyChangedEventArgs e)
         {
             if (obj == null) return;
             if (obj is Grid)
             {
+                var heightRatio = GetHeightRatio(obj);
                 var grid = obj as Grid;
                 var gs = e.NewValue as GridSplitter;
                 if (gs != null)
@@ -43,15 +62,16 @@ namespace DaxStudio.UI.AttachedProperties
                             grid,
                             (GridSplitter)_sender,
                             (bool)_e.NewValue,
-                            (bool)_e.OldValue);
+                            (bool)_e.OldValue,
+                            heightRatio);
                     };
                 }
             }
         }
 
         // Given: 
-        static Dictionary<DependencyObject, GridLength> oldValues = new Dictionary<DependencyObject, GridLength>();
-        private static void UpdateGrid(Grid grid, GridSplitter gridSplitter, bool newValue, bool oldValue)
+        //static Dictionary<DependencyObject, GridLength> oldValues = new Dictionary<DependencyObject, GridLength>();
+        private static void UpdateGrid(Grid grid, GridSplitter gridSplitter, bool newValue, bool oldValue, double heightRatio)
         {
             if (newValue)
             {
@@ -64,11 +84,19 @@ namespace DaxStudio.UI.AttachedProperties
                         int ridx = (int)gridSplitter.GetValue(Grid.RowProperty);
                         var prev = grid.RowDefinitions.ElementAt(GetPrevious(gridSplitter, ridx));
                         var curr = grid.RowDefinitions.ElementAt(GetNext(gridSplitter, ridx));
-                        if (oldValues.ContainsKey(prev) && oldValues.ContainsKey(curr))
-                        {
-                            prev.Height = oldValues[prev];
-                            curr.Height = oldValues[curr];
-                        }
+                        //if (oldValues.ContainsKey(prev) && oldValues.ContainsKey(curr))
+                        //{
+                        //    prev.Height = oldValues[prev];
+                        //    curr.Height = oldValues[curr];
+                        //}
+
+                        var prevHeight = prev.Height.Value;
+                        var newPrevHeight = prevHeight * (1 / (1 + heightRatio));
+                        var newNextHeight = newPrevHeight * heightRatio;
+                        System.Diagnostics.Debug.WriteLine($"PrevHeight: {prevHeight} NewPrevHeight: {newPrevHeight} NewNextHeight: {newNextHeight}");
+                        var prevUnits = prev.Height.GridUnitType;
+                        prev.Height = new GridLength( newPrevHeight, prevUnits);
+                        curr.Height = new GridLength( newNextHeight, prevUnits) ;
 
                         break;
                 }
@@ -89,11 +117,11 @@ namespace DaxStudio.UI.AttachedProperties
                             // Naively assumes only one type of collapsing!
                             case GridResizeBehavior.PreviousAndNext:
                             case GridResizeBehavior.PreviousAndCurrent:
-                                oldValues[prev] = prev.Height;
+                                //oldValues[prev] = prev.Height;
                                 // add both heights to the previous grid row
                                 prev.Height = new GridLength( prev.Height.Value + curr.Height.Value, prev.Height.GridUnitType);  // new GridLength(1.0, GridUnitType.Star);
 
-                                oldValues[curr] = curr.Height;
+                                //oldValues[curr] = curr.Height;
                                 curr.Height = new GridLength(0.0);
                                 break;
                         }
