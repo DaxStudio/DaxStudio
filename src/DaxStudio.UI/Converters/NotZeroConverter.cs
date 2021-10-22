4ffda02d6a853c3c9919da01d647b02fd5334ed7
dgosbell <dgosbell@users.noreply.github.com>
2021-10-22 18:30:31 +1100

    tab spinner update

---
+++ src/DaxStudio.UI/Converters/NotZeroConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DaxStudio.UI.Converters
+{
+    public class NotZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse((value ?? "").ToString(), out double val))
+            {
+                return Math.Abs(val) > 0.0;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}
