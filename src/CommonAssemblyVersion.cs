5397f7632e6d2feca2b7f07dd9da2cf190b68fca
dgosbell <dgosbell@users.noreply.github.com>
2021-11-23 15:59:29 +1100

    possible fix for corrupt fonts

--- src/CommonAssemblyVersion.cs
+++ src/CommonAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 // Note: the 4th section of the version is automatically incremented on the AppVeyor builds
-[assembly: AssemblyVersion(    "2.17.3.9999")]
-[assembly: AssemblyFileVersion("2.17.3.9999")]
+[assembly: AssemblyVersion(    "2.17.4.9999")]
+[assembly: AssemblyFileVersion("2.17.4.9999")]
