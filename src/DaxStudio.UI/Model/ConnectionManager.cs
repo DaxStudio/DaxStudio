072fac7a6f1bdcb4c5c168c0ac8ded495be6ea60
Marco Russo <marco.russo@loader.it>
2022-08-25 19:08:08 +0200

    Fix #894

--- src/DaxStudio.UI/Model/ConnectionManager.cs
+++ src/DaxStudio.UI/Model/ConnectionManager.cs
@@ -675,7 +675,7 @@ namespace DaxStudio.UI.Model
                 {
                     //string daxMeasureName = "[" + modelMeasure.Name + "]";
                     //string newExpression = resultExpression.Replace(daxMeasureName, " CALCULATE ( " + modelMeasure.Expression + " )");
-                    Regex daxMeasureRegex = new Regex(@"[^\w']?\[" + modelMeasure.Name.Replace("]","]]") + "]");
+                    Regex daxMeasureRegex = new Regex($@"[^\w']?\[{ modelMeasure.Name.Replace("]","]]")}]|'{modelMeasure.Table.DaxName}'\[{modelMeasure.Name.Replace("]", "]]")}]|{modelMeasure.Table.DaxName}\[{modelMeasure.Name.Replace("]", "]]")}]");
 
                     string newExpression = daxMeasureRegex.Replace(resultExpression, " CALCULATE ( " + modelMeasure.Expression + " )");
 
