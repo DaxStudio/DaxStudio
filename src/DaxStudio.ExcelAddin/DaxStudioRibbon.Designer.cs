908e4101cf09f74bbc17a7c85d58cd025f4c16f3
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-09-09 12:07:13 +1000

    updating addin icon

--- src/DaxStudio.ExcelAddin/DaxStudioRibbon.Designer.cs
+++ src/DaxStudio.ExcelAddin/DaxStudioRibbon.Designer.cs
@@ -38,6 +38,7 @@
             this.tab1 = this.Factory.CreateRibbonTab();
             this.group1 = this.Factory.CreateRibbonGroup();
             this.btnDax = this.Factory.CreateRibbonButton();
+            this.btnTest1 = this.Factory.CreateRibbonButton();
             this.btnTest = this.Factory.CreateRibbonButton();
             this.tab1.SuspendLayout();
             this.group1.SuspendLayout();
@@ -53,6 +54,7 @@
             // group1
             // 
             this.group1.Items.Add(this.btnDax);
+            this.group1.Items.Add(this.btnTest1);
             this.group1.Items.Add(this.btnTest);
             this.group1.Name = "group1";
             // 
@@ -65,6 +67,14 @@
             this.btnDax.ScreenTip = "Launch DAX Studio";
             this.btnDax.ShowImage = true;
             this.btnDax.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BtnDaxClick);
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.Label = "Test";
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.ScreenTip = "Test Button";
+            this.btnTest1.Visible = false;
+            this.btnTest1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnTest1_Click);
             // 
             // btnTest
             // 
@@ -92,6 +102,7 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton btnDax;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton btnTest;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnTest1;
     }
 
     partial class ThisRibbonCollection
