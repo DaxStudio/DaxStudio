40794df50d9961751fe7e5d7b4e15c03f72435bf
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-09-10 13:23:20 +1000

    improve Power BI desktop connection

--- src/DaxStudio.UI/Utils/PowerBIHelper.cs
+++ src/DaxStudio.UI/Utils/PowerBIHelper.cs
@@ -18,28 +18,48 @@ namespace DaxStudio.UI.Utils
     }
     public class PowerBIInstance
     {
-        public PowerBIInstance(string name, int port, EmbeddedSSASIcon icon)
+        public static readonly string[] PBIDesktopMainWindowTitleSuffixes = new string[]
+        {
+            // Different characters are used as a separator in the PBIDesktop window title depending on the current UI culture/localization
+            // See https://github.com/sql-bi/Bravo/issues/476
+
+            " \u002D Power BI Desktop", // Dash Punctuation - minus hyphen
+            " \u2212 Power BI Desktop", // Math Symbol - minus sign
+            " \u2011 Power BI Desktop", // Dash Punctuation - non-breaking hyphen
+            " \u2013 Power BI Desktop", // Dash Punctuation - en dash
+            " \u2014 Power BI Desktop", // Dash Punctuation - em dash
+            " \u2015 Power BI Desktop", // Dash Punctuation - horizontal bar
+        };
+
+        public PowerBIInstance(string windowTitle, int port, EmbeddedSSASIcon icon)
         {
             Port = port;
             Icon = icon;
             try
             {
-                var dashPos = name.LastIndexOf(" - ");
-                if (dashPos >= 0)
-                { Name = name.Substring(0, dashPos); }  // Strip "Power BI Designer" or "Power BI Desktop" off the end of the string
-                else
+                // Strip "Power BI Designer" or "Power BI Desktop" off the end of the string
+                foreach (var suffix in PBIDesktopMainWindowTitleSuffixes)
+                {
+                    var index = windowTitle.LastIndexOf(suffix);
+                    if (index >= 1)
+                    {
+                        Name = windowTitle.Substring(0,index-1).Trim();
+                    }
+                }
+
+                if (string.IsNullOrEmpty(Name))
                 {
                     if (port != -1)
                     {
-                        Log.Warning("{class} {method} {message} {dashPos}", "PowerBIInstance", "ctor", $"Unable to find ' - ' in Power BI title '{name}'", dashPos);
+                        Log.Warning("{class} {method} {message} {dashPos}", "PowerBIInstance", "ctor", $"Unable to find ' - ' in Power BI title '{windowTitle}'", dashPos);
                     }
-                    Name = name; 
+                    Name = windowTitle; 
                 }
             }
             catch (Exception ex)
             {
                 Log.Error("{class} {method} {message} {stacktrace}", "PowerBIInstance", "ctor", ex.Message, ex.StackTrace);
-                Name = name;
+                Name = windowTitle;
             }
         }
         public int Port { get; private set; }
