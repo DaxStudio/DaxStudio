1ed2832f2cf795915c39455b1657a71fbc281c5e
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-09-16 20:18:24 +1000

    fixing exe icons for file type registrations

--- src/DaxStudio.UI/Model/ServerTimesModel.cs
+++ src/DaxStudio.UI/Model/ServerTimesModel.cs
@@ -1,10 +1,6 @@
 ﻿using Caliburn.Micro;
 using DaxStudio.UI.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DaxStudio.UI.Model
 {
