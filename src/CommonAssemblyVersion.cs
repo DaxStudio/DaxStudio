ff2bbe180d11b2927887c88c92762d14e539cf10
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-09-23 15:36:01 +1000

    Update CommonAssemblyVersion.cs

--- src/CommonAssemblyVersion.cs
+++ src/CommonAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 // Note: the 4th section of the version is automatically incremented on the AppVeyor builds
-[assembly: AssemblyVersion(    "3.0.3.9998")]
-[assembly: AssemblyFileVersion("3.0.3.9998")]
+[assembly: AssemblyVersion(    "3.0.3.9999")]
+[assembly: AssemblyFileVersion("3.0.3.9999")]
