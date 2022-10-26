2bd156d88134762c34609d3339eb8e50e7dd6ef2
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-10-26 11:03:11 +1100

    Fix #979 improving color contrast of hidden metadata

---
+++ src/DaxStudio.UI/Converters/BoolToItalicConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace DaxStudio.UI.Converters
+{
+    internal class BoolToItalicConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var b = value as bool?;
+            if (b ?? false) return FontStyles.Normal;
+            return FontStyles.Italic;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
