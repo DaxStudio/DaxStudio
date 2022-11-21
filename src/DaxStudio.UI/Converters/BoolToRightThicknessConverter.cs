39db2eb903a875088e167934deab1863a55785fd
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-11-21 11:52:14 +1100

    Fix #994 Adjusting measure icon position for scrollbars

---
+++ src/DaxStudio.UI/Converters/BoolToRightThicknessConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DaxStudio.UI.Converters
+{
+    internal class BoolToRightThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool )
+            {
+                if (!(bool)value)
+                {
+                    var right = System.Convert.ToDouble(parameter);
+                    return new Thickness(0, 0, right, 0);
+                }
+                return new Thickness(0.0);
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
