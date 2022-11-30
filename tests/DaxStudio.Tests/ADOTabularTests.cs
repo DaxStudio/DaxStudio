abf761e92fc3acbd46d87d795e9157893d7fb419
Darren Gosbell <dgosbell@users.noreply.github.com>
2022-11-30 11:18:51 +1100

    fixing broken test

--- tests/DaxStudio.Tests/ADOTabularTests.cs
+++ tests/DaxStudio.Tests/ADOTabularTests.cs
@@ -779,7 +779,7 @@ namespace DaxStudio.Tests
 
 
         [TestMethod]
-        public void TestColumnRenaming()
+        public void TestColumnRenamingForDataBinding()
         {
             //ADOTabularConnection c = new ADOTabularConnection(ConnectionString, AdomdType.AnalysisServices);
             var dt = new DataTable();
@@ -796,12 +796,12 @@ namespace DaxStudio.Tests
             Assert.AreEqual("Column2", dt.Columns[2].ColumnName);
             Assert.AreEqual("Column3", dt.Columns[3].ColumnName);
             Assert.AreEqual("Column`4", dt.Columns[4].ColumnName,"spaces must be replaced with backticks");
-            Assert.AreEqual("Column``5", dt.Columns[5].ColumnName, "commas must be replaced with backticks");
-            Assert.AreEqual("[Measures] (test)", dt.Columns[6].Caption);
+            Assert.AreEqual("Column\\,`5", dt.Columns[5].ColumnName, "commas must be escaped with backslashes");
+            Assert.AreEqual("[Measures] (test)", dt.Columns[6].Caption,"Caption must not include opening and closing square braces");
         }
 
         [TestMethod]
-        public void TestMDXColumnRenaming()
+        public void TestMDXColumnRenamingForDataBinding()
         {
             //ADOTabularConnection c = new ADOTabularConnection(ConnectionString, AdomdType.AnalysisServices);
             var dt = new DataTable();
