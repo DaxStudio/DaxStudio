c1585e8a4ca769a28b04ab1310680527133e71cb
Darren Gosbell <dgosbell@users.noreply.github.com>
2023-08-21 09:42:24 +1000

    version bump

--- src/CommonAssemblyVersion.cs
+++ src/CommonAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 // Note: the 4th section of the version is automatically incremented on the AppVeyor builds
-[assembly: AssemblyVersion(    "3.0.10.9999")]
-[assembly: AssemblyFileVersion("3.0.10.9999")]
+[assembly: AssemblyVersion(    "3.0.11.9999")]
+[assembly: AssemblyFileVersion("3.0.11.9999")]
