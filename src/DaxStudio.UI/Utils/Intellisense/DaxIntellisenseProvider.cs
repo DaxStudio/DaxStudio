ed926e6225db39e887c40ebd4f444452f3b36658
Darren Gosbell <dgosbell@users.noreply.github.com>
2024-02-02 14:47:49 +1100

    reduce intellisense logging

--- src/DaxStudio.UI/Utils/Intellisense/DaxIntellisenseProvider.cs
+++ src/DaxStudio.UI/Utils/Intellisense/DaxIntellisenseProvider.cs
@@ -564,12 +564,13 @@ namespace DaxStudio.UI.Utils.Intellisense
 
         private DaxLineState ParseLine()
         {
-            //Log.Debug("{class} {method} {message}", "DaxIntellisenseProvider", "ParseLine", "start");
             string line = GetCurrentLine();
             int pos = _editor.CaretOffset>0 ? _editor.CaretOffset - 1 : 0;
             var loc = _editor.DocumentGetLocation(pos);
             var docLine = _editor.DocumentGetLineByOffset(pos);
+#if DEBUG
             Log.Debug("{class} {method} {line} col:{column} off:{offset}", "DaxIntellisenseProvider", "ParseLine", line,loc.Column, docLine.Offset);
+#endif
             return DaxLineParser.ParseLine(line, loc.Column, docLine.Offset);
         }
         
