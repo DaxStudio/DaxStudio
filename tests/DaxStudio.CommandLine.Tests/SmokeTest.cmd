3fed8e4c9abd121f895eac9de45609c4ab4260a1
Darren Gosbell <dgosbell@users.noreply.github.com>
2024-02-07 20:58:47 +1100

    Adding cmdline smoke test

---
+++ tests/DaxStudio.CommandLine.Tests/SmokeTest.cmd
@@ -0,0 +1,4 @@
+..\..\src\bin\debug\dscmd vpax c:\temp\test.vpax -s localhost\tab19 -d "Adventure Works"
+..\..\src\bin\debug\dscmd csv c:\temp\test.csv -s localhost\tab19 -d "Adventure Works" -q "EVALUATE 'Product'"
+..\..\src\bin\debug\dscmd xlsx c:\temp\test.xlsx -s localhost\tab19 -d "Adventure Works" -q "EVALUATE 'Product'"
+..\..\src\bin\debug\dscmd export csv c:\temp\cmdexport -s localhost\tab19 -d "Adventure Works"
