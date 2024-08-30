380ad2d83513b1da822a9212dadf578d311fbf5b
Darren Gosbell <dgosbell@users.noreply.github.com>
2024-08-30 12:04:17 +1000

    adding cmdline parameter test

---
+++ tests/DaxStudio.CommandLine.Tests/psTest.ps1
@@ -0,0 +1,14 @@
+﻿$query = @"
+EVALUATE
+FILTER( customer
+	, LEFT(customer[First Name], 1) = @letter
+)
+"@
+
+$cmd = "C:\Users\dgosbell\Downloads\DaxStudio_3_1_0_portable\dscmd.exe"
+$cmd = "C:\Users\dgosbell\source\repos\DaxStudio\src\bin\Debug\dscmd.exe"
+$server = "localhost\tab19"
+$database = "adventure works"
+$letter = 'm'
+
+&$cmd csv c:\temp\test-$letter.csv -s $server -d $database -q $query -m letter=$letter
