4de168294d5274088e973da821f4132c563ca3c8
Po-Wei Huang <pohuan@microsoft.com>
2025-04-09 09:40:45 -0700

    Fix more.

---
+++ src/ADOTabular/ADOTabularCalendar.cs
@@ -0,0 +1,36 @@
+﻿using ADOTabular.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ADOTabular
+{
+    
+    public class ADOTabularCalendar: IADOTabularObject
+    {
+        public ADOTabularCalendar(
+            ADOTabularTable table,
+            string name,
+            bool isVisible)
+        {
+            TimeUnits = new List<ADOTabularTimeUnit>();
+            TimeRelatedColumns = new List<ADOTabularColumn>();
+            Name = name;
+            table = table;
+            isVisible = isVisible;
+
+        }
+
+        public List<ADOTabularTimeUnit> TimeUnits { get; }
+
+        public List<ADOTabularColumn> TimeRelatedColumns { get; }
+        public override string DaxName
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        private ADOTabularTable table;
+    }
+}
---
+++ src/ADOTabular/ADOTabularTimeUnit.cs
@@ -0,0 +1,40 @@
+﻿using ADOTabular.Interfaces;
+using System;
+using System.Collections.Generic;
+using Microsoft.AnalysisServices.Tabular;
+
+namespace ADOTabular
+{
+
+    public class ADOTabularTimeUnit: IADOTabularObject
+    {
+        
+
+        public ADOTabularTimeUnit( ADOTabularColumn primaryColumn, ADOTabularColumn sortByColumn, List<ADOTabularColumn> associatedColumns)
+        {
+            Column = primaryColumn;
+            SortByColumn = sortByColumn;
+            AssociatedColumns = associatedColumns;
+        }
+
+        public ADOTabularColumn Column {get; }
+
+        public ADOTabularColumn SortByColumn { get; }
+
+        public List<ADOTabularColumn> AssociatedColumns { get; }
+
+        public string Name => Column.Name;
+        public string DaxName => Column.DaxName;
+        public ADOTabularObjectType ObjectType => ADOTabularObjectType.TimeUnit;
+        public bool IsVisible => true;
+        public string Description => Column.Description;
+
+        public Type SystemType => Column.SystemType;
+
+        public Microsoft.AnalysisServices.Tabular.DataType DataType => Column.DataType;
+
+        public MetadataImages MetadataImage => Column.MetadataImage;
+
+        public string TableName => Column.TableName;
+    }
+}
