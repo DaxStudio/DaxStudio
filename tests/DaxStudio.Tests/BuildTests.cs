1da862c4ad5aa15aa510c7f50f62140fc47ff750
Darren Gosbell <dgosbell@users.noreply.github.com>
2025-06-13 13:58:43 +1000

    updating dependencies

--- tests/DaxStudio.Tests/BuildTests.cs
+++ tests/DaxStudio.Tests/BuildTests.cs
@@ -15,7 +15,7 @@ namespace DaxStudio.Tests
         {
             var msg = "There is a hard coded AssemblyBinding in the standalone app.config and a reference in DAXStudio.UI to v5.0.0.0 to fix the static excel export issues #891";
             System.Reflection.Assembly assembly = System.Reflection.Assembly.Load("System.Runtime.CompilerServices.Unsafe");
-            Assert.AreEqual(new Version(6, 0, 2, 0), assembly.GetName().Version, msg);
+            Assert.AreEqual(new Version(6, 0, 3, 0), assembly.GetName().Version, msg);
         }
 
         [TestMethod]
@@ -23,7 +23,7 @@ namespace DaxStudio.Tests
         {
             var msg = "Added a reference in dscmd, daxstudio.exe and the test assemblies to v4.6.0.0 to fix the static excel export issues #891";
             System.Reflection.Assembly assembly = System.Reflection.Assembly.Load("System.Memory");
-            Assert.AreEqual(new Version(4, 0, 4, 0), assembly.GetName().Version, msg);
+            Assert.AreEqual(new Version(4, 0, 5, 0), assembly.GetName().Version, msg);
         }
     }
 }
