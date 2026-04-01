5f4f63865f3eebd3c931c62ef69e0f91c4a8e794
Darren Gosbell <dgosbell@users.noreply.github.com>
2026-04-01 11:57:15 +1100

    fixing crash when copying log messages with hyperlinks

--- src/DaxStudio.UI/Events/OutputMessage.cs
+++ src/DaxStudio.UI/Events/OutputMessage.cs
@@ -1,12 +1,9 @@
 ﻿using Caliburn.Micro;
 using DaxStudio.UI.ViewModels;
-using Microsoft.PowerBI.Api.Models;
 using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace DaxStudio.UI.Events
 {
@@ -21,33 +18,30 @@ namespace DaxStudio.UI.Events
         {
             get
             {
-                var doc = new FlowDocument();
-                doc.PagePadding = new Thickness(0);
-                doc.ColumnWidth = double.PositiveInfinity;
-                Paragraph para = new Paragraph();
-                para.Margin = new Thickness(0);
+                var doc = new FlowDocument
+                {
+                    PagePadding = new Thickness(0),
+                    ColumnWidth = double.PositiveInfinity
+                };
+                Paragraph para = new Paragraph
+                {
+                    Margin = new Thickness(0)
+                };
                 doc.Blocks.Clear();
                 doc.Blocks.Add(para);
                 // if we have a row and column then add a hyperlink to the location
                 if (Row != -1 && Column != -1)
                 {
-                    //var img = new Image();
-                    //img.SetResourceReference(Image.SourceProperty, "linkDrawingImage");
-                    //img.Width = 15;
-                    //img.Height = 15;
-                    //img.Margin = new Thickness(0, 4, 5, 0);
-                    ////img.Source = new DynamicResourceExtension("linkDrawingImage").ProvideValue(null) as ImageSource;
-                    //var container = new InlineUIContainer();
-                    //container.Child = img;
-                    //para.Inlines.Add(container);
-
                     var run2 = new Run($"Goto ({Row},{Column})");
-                    
-                    Hyperlink hlink = new Hyperlink(run2);
-                    hlink.ToolTip = $"Go to location ({Row},{Column})";
-                    hlink.Command = Parent.GotoLocation;
-                    hlink.CommandParameter = this;
-                    hlink.Cursor = Cursors.Hand;
+
+                    Hyperlink hlink = new Hyperlink(run2)
+                    {
+                        ToolTip = $"Go to location ({Row},{Column})",
+                        Cursor = Cursors.Hand
+                    };
+                    var parent = Parent;
+                    var self = this;
+                    hlink.Click += (s, e) => parent.GotoLocation.Execute(self);
                     para.Inlines.Add(hlink);
                     para.Inlines.Add(" ");
                 }
@@ -57,8 +51,6 @@ namespace DaxStudio.UI.Events
                 
                 return doc;
             }
-
-
         }
 
         public int Row { get; protected set; } = -1;
@@ -76,11 +68,15 @@ namespace DaxStudio.UI.Events
         {
             get
             {
-                var doc = new FlowDocument();
-                doc.PagePadding = new Thickness(0,2,0,2);
-                doc.ColumnWidth = double.PositiveInfinity;
-                Paragraph para = new Paragraph();
-                para.Margin = new Thickness(0);
+                var doc = new FlowDocument
+                {
+                    PagePadding = new Thickness(0, 2, 0, 2),
+                    ColumnWidth = double.PositiveInfinity
+                };
+                Paragraph para = new Paragraph
+                {
+                    Margin = new Thickness(0)
+                };
                 doc.Blocks.Clear();
                 doc.Blocks.Add(para);
                 Run run = new Run(Text);
@@ -89,28 +85,20 @@ namespace DaxStudio.UI.Events
                 // if we have a row and column then add a hyperlink to the location
                 if (!string.IsNullOrEmpty(FolderPath))
                 {
-                    //var img = new Image();
-                    //img.SetResourceReference(Image.SourceProperty, "linkDrawingImage");
-                    //img.Width = 15;
-                    //img.Height = 15;
-                    //img.Margin = new Thickness(0, 4, 5, 0);
-                    //var container = new InlineUIContainer();
-                    //container.Child = img;
-                    //para.Inlines.Add(container);
-
                     var run2 = new Run($"Open Folder");
 
-                    Hyperlink hlink = new Hyperlink(run2);
-                    hlink.ToolTip = $"Open {FolderPath}";
-                    hlink.Command = Parent.OpenFolder;
-                    hlink.CommandParameter = this;
-                    hlink.Cursor = Cursors.Hand;
+                    Hyperlink hlink = new Hyperlink(run2)
+                    {
+                        ToolTip = $"Open {FolderPath}",
+                        Cursor = Cursors.Hand
+                    };
+                    var parent = Parent;
+                    var self = this;
+                    hlink.Click += (s, e) => parent.OpenFolder.Execute(self);
                     para.Inlines.Add(new LineBreak());
                     para.Inlines.Add(hlink);
                     
                 }
-
-                
 
                 return doc;
             }
